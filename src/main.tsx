5f09795b1e1330aeb3eea09d98522b5a3ac1c401
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-10-01 22:06:36 +0300

    Add time log

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import * as wasm from "wasm-pkg";
 
+console.time('wasm init');
 console.log(wasm.parse('{ "x": 10, "y": 2 }'));
+console.timeEnd('wasm init');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
