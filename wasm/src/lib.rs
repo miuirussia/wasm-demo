3c5159638377579466142b8f0d201df571bc1279
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-10-02 17:07:08 +0300

    Update tsify to latest version and use native bigints

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,7 +1,6 @@
 mod utils;
 
 use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::Serializer;
 use tsify::Tsify;
 use wasm_bindgen::prelude::*;
 
@@ -11,15 +10,14 @@ use crate::utils::set_panic_hook;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[derive(Tsify, Serialize, Deserialize)]
+#[tsify(into_wasm_abi,large_number_types_as_bigints,missing_as_null)]
 pub struct Point {
-  #[tsify(type = "bigint")]
   x: i64,
-  #[tsify(type = "bigint")]
   y: i64,
 }
 
-#[wasm_bindgen(unchecked_return_type = "Point")]
-pub fn parse(s: &str) -> JsValue {
+#[wasm_bindgen]
+pub fn parse(s: &str) -> Point {
   set_panic_hook();
-  (&serde_json::from_str::<Point>(s).unwrap()).serialize(&Serializer::new().serialize_large_number_types_as_bigints(true)).unwrap()
+  serde_json::from_str::<Point>(s).unwrap()
 }
